10889e5fc6c60fbd89a3eeb7c7850053fe3c3bb9
Gaws21 <gawangilbert@gmail.com>
2024-02-15 14:02:31 -0300

    feat(style): update css design in search table

--- bolha_dev/__init__.py
+++ bolha_dev/__init__.py
@@ -16,6 +16,7 @@ class Joblinks(db.Model):
     __tablename__ = 'job_link'
     _ = db.Column(db.Integer, primary_key=True)
     job_link = db.Column(db.String(100), nullable=False)
+    job_name = db.Column(db.String(100), nullable=False)
 
 def init_app():
 
