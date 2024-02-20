71f8f95688c22dcf12374073e19719eb0137c3b6
Gaws21 <gawangilbert@gmail.com>
2024-02-19 23:20:57 -0300

    fix(app):remove create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
 
-def create_app(config):
-    app = Flask(__name__)
-    app.config.from_object(config)
-    
-    with app.app_context():
-        from . import routes
-        return app
+app = Flask(__name__)
+
+with app.app_context():
+    from . import routes
